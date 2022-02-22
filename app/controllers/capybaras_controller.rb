c29abee2c85aef996c6bbf6be2cd79da297b3a01
Frank Obeng <obengfan@gmail.com>
2022-02-22 14:53:43 +0100

    added create and new method

--- app/controllers/capybaras_controller.rb
+++ app/controllers/capybaras_controller.rb
@@ -1,5 +1,4 @@
 class CapybarasController < ApplicationController
-
   def index
     @capybaras = Capybara.all
   end
@@ -8,4 +7,22 @@ class CapybarasController < ApplicationController
     @capybara = Capybara.find(params[:id])
   end
 
+  def new
+    @capybara = Capybara.new
+  end
+
+  def create
+    @capybara = Capybara.new(capybara_params)
+
+    if @capybara.save
+      redirect_to @capybara, notice: 'New Capybara was successfully created.'
+    else
+      render :new
+    end
+  end
+  private
+
+  def capybara_params
+    arams.require(:capybara).permit(:name, :description)
+  end
 end

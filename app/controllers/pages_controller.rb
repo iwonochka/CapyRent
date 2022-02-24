0fcf11b499580163e84708b8ca1fc6ca7c51747b
Elena Teske <elenateske@gmx.net>
2022-02-24 13:01:20 +0100

    navbar raw and capy photos from db

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,14 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
+
+
   def home
+    @capybaras = Capybara.all
+  end
+
+  private
+
+  def capybara_params
+    params.require(:capybara).permit(:name, :description, :price, :photo)
   end
 end
